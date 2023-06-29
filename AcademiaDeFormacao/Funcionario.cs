1e1a51286c8a2bbdf16ea39fc839907e7c979e18
Claudio Silva <Claudio.Silva.T0123173@edu.atec.pt>
2023-06-29 20:38:15 +0100

    Classes

---
+++ AcademiaDeFormacao/Funcionario.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
+
+namespace AcademiaDeFormacao
+{
+    internal class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Morada { get; set; }
+        public string Contacto { get; set; }
+        public DateTime DataFimContrato { get; set; }
+        public DateTime DataRegistoCriminal { get; set; }
+
+        public Funcionario(int id, string nome, string morada, string contacto, DateTime fimContrato, DateTime registoCriminal)
+        {
+            Id = id;
+            Nome = nome;
+            Morada = morada;
+            Contacto = contacto;
+            DataFimContrato = fimContrato;
+            DataRegistoCriminal = registoCriminal;
+        }
+        public override string ToString()
+         {
+            return $"Novo Funcionario criado! Id: {Id}";
+         }
+
+    }
+
+
+}
